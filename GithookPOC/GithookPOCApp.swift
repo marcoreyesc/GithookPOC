6aa77d53637644870d68e7184422297301afd6f6
marco reyes <marco.reyes@globant.com>
2022-09-05 11:19:53 -0500

    Initial Commit

---
+++ GithookPOC/GithookPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithookPOCApp.swift
+//  GithookPOC
+//
+//  Created by Marco Antonio Reyes Castro on 05/09/22.
+//
+
+import SwiftUI
+
+@main
+struct GithookPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
