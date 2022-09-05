3912b38d6d3f6f36dc66c862455001b5b2132629
marco reyes <marco.reyes@globant.com>
2022-09-05 11:33:26 -0500

    our first commit

--- GithookPOC/ContentView.swift
+++ GithookPOC/ContentView.swift
@@ -11,11 +11,13 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
 }
