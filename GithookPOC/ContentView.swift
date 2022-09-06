1f67570b522371f146f490227d6eb9aab7023075
marco reyes <marco.reyes@globant.com>
2022-09-06 16:17:52 -0500

    our fail test commit 18

--- GithookPOC/ContentView.swift
+++ GithookPOC/ContentView.swift
@@ -11,13 +11,11 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
-
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View{
+    static var previews: some View {
         ContentView()
-
     }
 }
