ee9ba56b81bf2b0535ad6992e23a5857ece67107
marco reyes <marco.reyes@globant.com>
2022-09-08 14:29:32 -0500

    test 13

--- .githooks/scripts/run-tests.bash
+++ .githooks/scripts/run-tests.bash
@@ -8,7 +8,7 @@ xcodebuild \
   -sdk iphonesimulator \
   -destination 'platform=iOS Simulator,name=iPhone 11,OS=15.5' \
   -quiet \
-  test
+  test > $PROJECT_ROOT/.githooks/output/tests.result
 if [ $? -ne 0 ]; then
   echo "Test failed"
   exit 1
